6957a3f8920bae18cef7f83a98ef9e7eb28793b8
malikali001 <malik.alihussain001@gmail.com>
2024-11-08 10:04:25 +0500

    implement route to calculate profits

--- base/models/sale.py
+++ base/models/sale.py
@@ -8,6 +8,7 @@ from .product import Product
 class Sale(models.Model):
     date = models.DateTimeField(default=timezone.now())
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    cost_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     quantity = models.PositiveIntegerField()
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
--- product/serializers/sale_serializer.py
+++ product/serializers/sale_serializer.py
@@ -1,16 +1,19 @@
 from rest_framework import serializers
 
-from base.models import Inventory, Sale
+from base.models import Inventory, Product, Sale
 
 
 class SaleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sale
-        fields = ["id", "price", "quantity", "product"]
+        fields = ["id", "price", "cost_price", "quantity", "product"]
 
     def create(self, validated_data):
         user = self.context["request"].user
         validated_data["user"] = user
+
+        product = Product.objects.get(pk=validated_data["product"].id)
+        validated_data["cost_price"] = product.price
         sale = Sale(**validated_data)
 
         inventory = Inventory.objects.get(product_id=validated_data["product"].id)
